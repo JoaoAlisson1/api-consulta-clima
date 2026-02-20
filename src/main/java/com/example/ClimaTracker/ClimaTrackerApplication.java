2aa8f7a103ca90db38bda6bc09ee9f7fcc7a7ed7
Joao Alisson <joaoalissonflores@hotmail.com>
2026-02-20 01:04:02 -0300

    chore: Enviando arquivos iniciais

---
+++ src/main/java/com/example/ClimaTracker/ClimaTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClimaTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimaTrackerApplication.class, args);
+	}
+
+}
