6462022a82dd4e150ae3354692178bdaca057ec9
Joao Alisson <joaoalissonflores@hotmail.com>
2026-02-21 04:59:26 -0300

    feat(controller): adiciona endpoints POST e GET

---
+++ src/main/java/com/example/ClimaTracker/controller/ConsultaClimaController.java
@@ -0,0 +1,29 @@
+package com.example.ClimaTracker.controller;
+
+import com.example.ClimaTracker.dto.ConsultaClimaRequestDTO;
+import com.example.ClimaTracker.dto.ConsultaClimaResponseDTO;
+import com.example.ClimaTracker.service.ConsultaClimaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/clima")
+
+public class ConsultaClimaController {
+
+    private final ConsultaClimaService service;
+
+    @PostMapping
+    public ConsultaClimaResponseDTO salvar(@RequestBody ConsultaClimaRequestDTO request) {
+        return service.salvar(request);
+    }
+
+    @GetMapping
+    public List<ConsultaClimaResponseDTO> listar() {
+        return service.listar();
+    }
+
+}
