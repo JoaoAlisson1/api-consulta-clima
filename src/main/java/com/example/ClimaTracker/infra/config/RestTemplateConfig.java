2fa7e6d09ceff22fe94036ccd687f22369304ca8
Joao Alisson <joaoalissonflores@hotmail.com>
2026-02-21 21:41:23 -0300

    feat(config): Cria classe de configuracao do RestTemplate

---
+++ src/main/java/com/example/ClimaTracker/infra/config/RestTemplateConfig.java
@@ -0,0 +1,13 @@
+package com.example.ClimaTracker.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplateConfig restTemplate() {
+        return new RestTemplateConfig();
+    }
+}
