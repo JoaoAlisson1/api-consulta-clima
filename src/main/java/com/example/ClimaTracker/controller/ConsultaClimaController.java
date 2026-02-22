d9086f614b0040b32f24f3feee9aea4a1364f264
Joao Alisson <joaoalissonflores@hotmail.com>
2026-02-21 21:23:20 -0300

    chore(pom.xml): Adiciona spring validation

--- src/main/java/com/example/ClimaTracker/controller/ConsultaClimaController.java
+++ src/main/java/com/example/ClimaTracker/controller/ConsultaClimaController.java
@@ -3,6 +3,7 @@ package com.example.ClimaTracker.controller;
 import com.example.ClimaTracker.dto.ConsultaClimaRequestDTO;
 import com.example.ClimaTracker.dto.ConsultaClimaResponseDTO;
 import com.example.ClimaTracker.service.ConsultaClimaService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +18,7 @@ public class ConsultaClimaController {
     private final ConsultaClimaService service;
 
     @PostMapping
-    public ConsultaClimaResponseDTO salvar(@RequestBody ConsultaClimaRequestDTO request) {
+    public ConsultaClimaResponseDTO salvar(@RequestBody @Valid ConsultaClimaRequestDTO request) {
         return service.salvar(request);
     }
 
